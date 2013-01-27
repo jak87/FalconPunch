9e0595d984576cf35cc202fe2232aa9206fa59e6
Svetlin Kalaydjiev <svetlin@curzon.bu.edu>
2013-01-27 15:44:11 -0500

    Adding dummy code in lib

---
+++ lib/dummy.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void
+dummy_hello(void)
+{
+    printf("dummy_hello(): Hello from lib\n");
+}
---
+++ lib/dummy.h
@@ -0,0 +1,5 @@
+#ifndef __FALCONPUNCH_DUMMY_H__
+#define __FALCONMUNCH_DUMMY_H__
+
+void dummy_hello(void);
+#endif
