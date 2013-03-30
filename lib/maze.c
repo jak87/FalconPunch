0b15b17105cf1c64466f298d088a96a7dde0175d
Svetlin Kalaydjiev <skalaydj@akamai.com>
2013-03-30 16:23:22 -0400

    implementation of cell marshall/unmarshall + dummy test of board marshall/unmarshall

--- lib/maze.c
+++ lib/maze.c
@@ -88,3 +88,42 @@ void dump(){
     printf("%s\n", buf);
   }
 }
+
+extern int
+maze_marshall_cell(Proto_Session *s, Cell *cell)
+{
+  int rc = 1;
+  rc = proto_session_marshall_int(s, cell->x);
+  if (rc != 1) return rc;
+  rc = proto_session_marshall_int(s, cell->y);
+  if (rc != 1) return rc;
+  rc = proto_session_marshall_char(s, cell->type);
+  if (rc != 1) return rc;
+  rc = proto_session_marshall_short(s, cell->team);
+  return rc;
+}
+
+extern int
+maze_unmarshall_cell(Proto_Session *s, int offset, Cell *cell)
+{
+  offset = proto_session_body_unmarshall_int(s, offset, &(cell->x));
+  if (offset < 0) return offset;
+  offset = proto_session_body_unmarshall_int(s, offset, &(cell->y));
+  if (offset < 0) return offset;
+  offset = proto_session_body_unmarshall_char(s, offset, &(cell->type));
+  if (offset < 0) return offset;
+  offset = proto_session_body_unmarshall_short(s, offset, &(cell->team));
+  return offset;
+}
+
+extern int
+maze_marshall_board(Proto_Session *s)
+{
+  return maze_marshall_cell(s, &(Board.cells[0][0]));
+}
+
+extern int
+maze_unmarshall_board(Proto_Session *s, int offset)
+{
+  return maze_unmarshall_cell(s, offset, &(Board.cells[0][0]));
+}
