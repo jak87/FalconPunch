9fc200eabd42e49aecb28f06868500ec32439b5d
Jack Kanarek <jak87@curzon.bu.edu>
2013-05-02 19:34:47 -0400

    just printfs

--- client/client.c
+++ client/client.c
@@ -121,6 +121,8 @@ update_event_handler(Proto_Session *s)
 
   ui_update(ui);
 
+  printf("UI Updated!\n");
+
   return 1;
 }
 
@@ -205,25 +207,27 @@ ui_keypress(UI *ui, SDL_KeyboardEvent *e)
     if (sym == SDLK_LEFT && mod == KMOD_NONE) {
       fprintf(stderr, "%s: move left\n", __func__);
       rc = proto_client_move(globals.client_inst->ph, MOVE_LEFT);
-      ui_center_on_player(ui);
+      //ui_center_on_player(ui);
       return rc;
     }
     if (sym == SDLK_RIGHT && mod == KMOD_NONE) {
       fprintf(stderr, "%s: move right\n", __func__);
       rc = proto_client_move(globals.client_inst->ph, MOVE_RIGHT);
-      ui_center_on_player(ui);
+      //ui_center_on_player(ui);
       return rc;
     }
     if (sym == SDLK_UP && mod == KMOD_NONE)  {  
       fprintf(stderr, "%s: move up\n", __func__);
       rc = proto_client_move(globals.client_inst->ph, MOVE_UP);
-      ui_center_on_player(ui);
+      printf("Attempting to center...\n");
+      //ui_center_on_player(ui);
+      printf("Centered!\n");
       return rc;
     }
     if (sym == SDLK_DOWN && mod == KMOD_NONE)  {
       fprintf(stderr, "%s: move down\n", __func__);
       rc = proto_client_move(globals.client_inst->ph, MOVE_DOWN);
-      ui_center_on_player(ui);
+      //ui_center_on_player(ui);
       return rc;
     }
     if (sym == SDLK_r && mod == KMOD_NONE)  {  
