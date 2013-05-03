5c5860e577efe99e0441a81ec1c9bbb1d07ec0cf
Jack Kanarek <jak87@csa2.bu.edu>
2013-05-03 19:49:05 -0400

    Made pickup way more readable

--- lib/game_control.c
+++ lib/game_control.c
@@ -418,38 +418,33 @@ extern int game_move_player(Player* p, Player_Move direction)
   return didMove;
 }
 
-// Can't have more than 2 items on one cell
+
 extern int game_player_pickup(Player* p) {
-  if(flag_0.x == p->x && flag_0.y == p->y) {
-    if(flag_0.team == p->team)
-      GameState.players[p->team][p->id]->state = PLAYER_OWN_FLAG;
-    else
-      GameState.players[p->team][p->id]->state = PLAYER_OPPONENT_FLAG;
-    printf("Player %d (team %d) has picked up flag 0!\n",
-	   p->id, p->team);
-    return 1;
+  Object * o;
+  int i;
+
+  for (i = 0; i < 4; i++) {
+
+    o = &(GameState.objects[i]);    
+    if ((o->x == p->x) && (o->y == p->y)) {
+
+      // Checks to see if it's a flag
+      if (i < 2) {
+	if(o->team == p->team)
+	  GameState.players[p->team][p->id]->state = PLAYER_OWN_FLAG;
+	else
+	  GameState.players[p->team][p->id]->state = PLAYER_OPPONENT_FLAG;
+      }
+
+      // If not, set the player to have the correct shovel
+      else 
+	GameState.players[p->team][p->id]->shovel = (i-2);
+      
+      return 1;
+    }
   }
-  else if(flag_1.x == p->x && flag_1.y == p->y) {
-    if(flag_1.team == p->team)
-      GameState.players[p->team][p->id]->state = PLAYER_OWN_FLAG;
-    else
-      GameState.players[p->team][p->id]->state = PLAYER_OPPONENT_FLAG;
-    printf("Player %d (team %d) has picked up flag 1!\n",
-	   p->id, p->team);
-    return 1;
-  } 
-  else if(shovel_0.x == p->x && shovel_0.y == p->y) {
-    GameState.players[p->team][p->id]->shovel = 1;
-    printf("Player %d (team %d) has picked up Shovel 0!\n",
-	   p->id, p->team);
-    return 1;
-  } 
-  else if(shovel_1.x == p->x && shovel_1.y == p->y) {
-    GameState.players[p->team][p->id]->shovel = 2;
-    printf("Player %d (team %d) has picked up Shovel 1!\n",
-	   p->id, p->team);
-    return 1;
-  } 
+
+  // Nothing to pick up
   return 0;
 }
 
